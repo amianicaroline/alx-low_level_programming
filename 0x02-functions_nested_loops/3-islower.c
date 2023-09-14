ab94e411e373305277622a8f42da3a1cbec7fc18
amiani <amiani@deriv.tk>
2023-09-14 04:05:51 +0300

    commited

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _islower - checks if a character is a lowercase letter in the English alphabet.
+ * _islower - checks if a character is a lowercase
+ * letter in the English alphabet.
  * @ch: character to check.
  *
  * Return: 1 if it is a lowercase letter, 0 otherwise.
