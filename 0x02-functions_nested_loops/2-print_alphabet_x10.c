21204c96f350ac9cbc3590313e3bc32358e84c8a
amiani <amiani@deriv.tk>
2023-09-14 03:55:32 +0300

    commited

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * print_alphabet - This function prints the English alphabet from 'a' to 'z'.
+ *
+ * Return: This function does not return any value.
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
+
+/**
  * print_alphabet_x10 - This function prints the English alphabet from 'a' to 'z' ten times.
  *
  * Return: This function does not return any value.
